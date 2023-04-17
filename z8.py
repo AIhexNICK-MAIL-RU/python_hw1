525efc5d686a8f84330b821783513a96bb432820
Alexeev1 <87630739+Alexeev1@users.noreply.github.com>
2023-04-17 07:14:32 +0300

    Update z8.py

--- z8.py
+++ z8.py
@@ -12,7 +12,7 @@
 n = int(input())
 m = int(input())
 k = int(input())
-if n // 2 == 0 or m // 2 == 0 and k // 2 == 0:
+if k < n * m and (k % n == 0 or k % m == 0):
     print('yes')
 else:
     print('no')
